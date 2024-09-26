e7d28079302972bb88b8c2ac07f4d7235c31e12f
Aditya Kumar Chauhan <181999298+VilenAadi@users.noreply.github.com>
2024-09-26 09:40:06 +0530

    Add files via upload

---
+++ aditya/p1.py
@@ -0,0 +1,11 @@
+#list
+#list areused to store multiple items in a single variable.
+#list are one of 4 built in data types in python to store collections of data
+#the other 3 are tuple, setdictionar
+#all wuth different quantities and usage
+#mutable objects in python
+#mutable objects are of type python listlist are created using square bracktes
+mylist=["apple", "banana", "cherry"]
+print(mylist)
+mylist.append("mango")
+print(mylist)
