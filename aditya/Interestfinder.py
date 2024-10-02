ebfc9ac26fe999e68572d260ee7ab62cb9ff3b8e
Aditya Kumar Chauhan <181999298+VilenAadi@users.noreply.github.com>
2024-10-02 20:52:35 +0530

    Update Interestfinder.py

--- aditya/Interestfinder.py
+++ aditya/Interestfinder.py
@@ -1,3 +1,6 @@
+#female age<58 interest rate is 8.2 anf if age>58 and age<100 then interest rate is 9.2
+#male age<58 interest rate is 8.4 anf if age>58 and age<100 then interest rate is 9.4
+
 gender=input("enter gender M/F: ")
 age=int(input("enter age:"))
 if (gender=="F" and 1<age<58):
