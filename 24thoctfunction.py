a74ce6b9d26b5bc3c3e3943d573521671fe5f1ac
Aditya Kumar Chauhan <181999298+VilenAadi@users.noreply.github.com>
2024-10-24 10:39:47 +0530

    Add files via upload

---
+++ 24thoctfunction.py
@@ -0,0 +1,30 @@
+# def message():
+#     print("This is Aditya Balraj")
+# message()
+# def message():
+#     return 'This is Aditya Balraj'
+# s1=message()
+# print(s1)
+# def message(name,age):
+#     print('Hi',name)
+#     print('Age is',age)
+# message('Aditya',20)
+# def message(name,section,course,batch):
+#     print('My name is',name)
+#     print('My section is',section)
+#     print('My course is',course)
+#     print('my batch is',batch)
+# name=input("Enter Your name:")
+# section=input("Enter your section:")
+# course=input("Enter your course:")
+# batch=input("Enter your batch:")
+# message(name,section,course,batch)
+num=int(input("Enter A number: "))
+def choice(square,cube):
+    if choice=='square':
+        square= num*num
+    else:
+        cube=num*num*num
+    return square,cube
+print(choice(square,cube))
+choice()
